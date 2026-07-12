08f2b2db6cbb8fb756a6981ee8eed95d0b468a6c
ATHARV <atharvtambe263@gmail.com>
2026-07-12 11:05:32 +0530

    database connection

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from app.config import settings
 from app.routers import home
 from app.routers import auth
 from app.routers import contact
+
+from sqlalchemy import text
+from app.database import engine
 
 app = FastAPI(title="Expense Tracker")
 
@@ -12,3 +15,18 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 app.include_router(home.router)
 app.include_router(auth.router)
 app.include_router(contact.router)
+
+
+
+print(settings.DATABASE_URL)
+
+@app.get("/db-test")
+def db_test():
+
+    with engine.connect() as connection:
+
+        result = connection.execute(text("SELECT version();"))
+
+        return {
+            "database": result.scalar()
+        }
